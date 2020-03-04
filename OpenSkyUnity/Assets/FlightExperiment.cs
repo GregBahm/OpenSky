c58b397a045575551f46cde7b8458b0ffbff02db
derp derp <workingbahm@yahoo.com.sg>
2020-03-04 09:33:21 -0800

    Continuing flight development.

--- OpenSkyUnity/Assets/FlightExperiment.cs
+++ OpenSkyUnity/Assets/FlightExperiment.cs
@@ -10,7 +10,7 @@ public class FlightExperiment : MonoBehaviour
     public GameObject Ship;
     public int Iterations;
     private GameObject[] shipIterations;
-
+    
     private void Start()
     {
         shipIterations = CreateShipIterations().ToArray(); ;
@@ -18,38 +18,53 @@ public class FlightExperiment : MonoBehaviour
 
     private void Update()
     {
-        CurrentMomentum = Vector3.zero;
-        Vector3 targetPos = Waypoint.transform.position;
+        AnchoredPathSegment currentPath = GetCurrentPath();
         for (int i = 1; i < Iterations; i++)
         {
-
-            MoveEntity(shipIterations[i -1].transform, shipIterations[i].transform, targetPos);
         }
     }
-
-    Vector3 CurrentMomentum;
-    public float Acceleration;
-    public float MaxAngleChange;
-    public float MaxThrust;
-
-    public void MoveEntity(Transform priorTransform, Transform nextTransform, Vector3 targetPosition)
-    {
-        Vector3 toTarget = targetPosition - priorTransform.position;
-        Quaternion lookRot = Quaternion.LookRotation(toTarget);
-        nextTransform.rotation = Quaternion.RotateTowards(priorTransform.rotation, lookRot, MaxAngleChange);
-        CurrentMomentum = CurrentMomentum + nextTransform.forward * Acceleration;
-        if (CurrentMomentum.magnitude > MaxThrust)
-        {
-            CurrentMomentum = CurrentMomentum.normalized * MaxThrust;
-        }
-        nextTransform.position = priorTransform.position + CurrentMomentum;
-    }
-
+    
     private IEnumerable<GameObject> CreateShipIterations()
     {
         for (int i = 0; i < Iterations; i++)
         {
             yield return Instantiate(Ship);
         }
+    }
+}
+
+public struct AnchoredPathSegment
+{
+    public Vector3 Start { get; }
+    public Vector3 StartForward { get; }
+    public Vector3 End { get; }
+    public Vector3 EndForward { get; }
+
+    private readonly Vector3 startAnchor;
+    private readonly Vector3 endAnchor;
+
+    public AnchoredPathSegment(Vector3 start, 
+        Vector3 startForward, 
+        Vector3 end, 
+        Vector3 endForward) : this()
+    {
+        Start = start;
+        StartForward = startForward;
+        End = end;
+        EndForward = endForward;
+
+        float length = (Start - End).magnitude;
+        startAnchor = StartForward * length * (1f / 3);
+        endAnchor = -EndForward * length * (1f / 3);
+    }
+
+    public Vector3 PlotPosition(float param)
+    {
+        Vector3 ab = Vector3.Lerp(Start, startAnchor, param);
+        Vector3 bc = Vector3.Lerp(startAnchor, endAnchor, param);
+        Vector3 cd = Vector3.Lerp(endAnchor, End, param);
+        Vector3 abc = Vector3.Lerp(ab, bc, param);
+        Vector3 bcd = Vector3.Lerp(bc, cd, param);
+        return Vector3.Lerp(abc, bcd, param);
     }
 }
